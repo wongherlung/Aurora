847337ceb9f6f18d18f156e66236537ec140e2d9
wongherlung <wongherlung@gmail.com>
2015-11-01 23:53:32 +0800

    added carousel

--- app/components/Card/index.jsx
+++ app/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './style';
 import classNames from 'classnames';
+import Slider from 'react-slick';
 
 export default class Card extends React.Component {
     render() {
@@ -9,9 +10,28 @@ export default class Card extends React.Component {
             'col-sm-6': true,
             'card-container': true
         });
+
+        var settings = {
+            dots: true,
+            arrows: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+
         return <div className={cardClass}>
             <div className="card-inner-container">
-                
+                <div className="card-image">
+                    <Slider {...settings}>
+                        <div><img src="http://lorempixel.com/300/300/"/></div>
+                        <div><img src="http://lorempixel.com/300/300/"/></div>
+                        <div><img src="http://lorempixel.com/300/300/"/></div>
+                    </Slider>
+                </div>
+                <div className="card-controls">
+
+                </div>
             </div>
         </div>;
     }
